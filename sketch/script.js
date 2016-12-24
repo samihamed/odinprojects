c6369122b483bc789c98c600c431875b66ef3e5f
Sami Hamed <sami.k.hamed@googlemail.com>
2016-12-24 11:06:41 +0100

    add sketch to general odinprojects repo

--- sketch/script.js
+++ sketch/script.js
@@ -4,32 +4,33 @@ $(document).ready(function(){
 
 var userInput = 10;
 
-function newGrid(){
-  userInput = prompt('Pick a number between 1 and 25', 'please enter number');
-  startGrid();
-};
-
 function startGrid(){
 var numBlocks = userInput * userInput;
+var boxDimension = 800/userInput;
 function createGrid(numBlocks) {
   $('.container').find('.box').remove();
-  if(userInput <= 25){
+  if(userInput <= 50){
   for (i = 0; i <= numBlocks; i++) {
-    var boxDimension = 800/userInput;
-    $('.box').css({'width': boxDimension + 'px', 'height': boxDimension + 'px'}).on('mouseenter', function(){
-      $(this).css({'background-color': 'black'});
-    });
+    $('.box').css({'width': boxDimension + 'px', 'height': boxDimension + 'px'});
     $('.container').append("<div class='box'></div>");
   }}
   else {
     alert(userInput + ' is too big. Here is 10*10 instead');
     userInput = 10;
-    numBlocks = userInput * userInput;
-    createGrid(numBlocks);
+    startGrid();
   }};
 createGrid(numBlocks);
+$('.box').on('mouseenter', function(){
+  $(this).css({'background-color': 'black'})
+});
+$('.header').find('.count').text(userInput + ' * ' + userInput);
 };
 
 function resetGrid(){
   $('.container').find('.box').css({'background-color': 'white'});
+};
+
+function newGrid(){
+  userInput = prompt('Pick a number between 1 and 25', 'please enter number');
+  startGrid();
 };
